8f6e8485a87a29ff8fb512a1345f6704e55a50dc
Balázs Urbán <balazs.urban@stud.uni-corvinus.hu>
2021-10-01 22:14:41 +0200

    Projekt

--- UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/Resource1.Designer.cs
@@ -86,5 +86,13 @@ namespace UserMaintenance {
                 return ResourceManager.GetString("LastName", resourceCulture);
             }
         }
+
+        internal static string File
+        {
+            get
+            {
+                return ResourceManager.GetString("File", resourceCulture);
+            }
+        }
     }
 }
